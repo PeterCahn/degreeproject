05576275dc53bd7860fc228c68e10d7d193ba5c3
Pietro Cannalire <pietro@sky1.it.kth.se>
2017-06-05 17:19:13 +0200

    Working average computation over Kafka streams

--- applications/thesis01/src/main/scala/com/pietro/thesis/KafkaDataExample.scala
+++ applications/thesis01/src/main/scala/com/pietro/thesis/KafkaDataExample.scala
@@ -14,13 +14,16 @@ import org.apache.kafka.clients.producer.{KafkaProducer, ProducerConfig, Produce
 
 import org.apache.spark.SparkConf
 import org.apache.spark.streaming._
+import org.apache.spark.streaming.dstream._
 import org.apache.spark.streaming.kafka010._
 import org.apache.kafka.common.serialization.StringDeserializer
 import org.apache.spark.streaming.kafka010.LocationStrategies.PreferConsistent
 import org.apache.spark.streaming.kafka010.ConsumerStrategies.Subscribe
 import org.apache.kafka.clients.producer._
+import org.apache.spark.rdd.RDD
 
 import org.apache.log4j._
+import scala.concurrent.duration._
 
 /**
  * Consumes messages from one or more topics in Kafka and does wordcount.
@@ -37,40 +40,64 @@ import org.apache.log4j._
  */
 object KafkaWordCount {
   def main(args: Array[String]) {
-    if (args.length < 4) {
-      System.err.println("Usage: KafkaWordCount <zkQuorum> <group> <topics> <numThreads>")
+    if (args.length < 3) {
+      System.err.println("Usage: KafkaWordCount <zkQuorum> <group> <topics>")
       System.exit(1)
     }
 
 //    StreamingExamples.setStreamingLogLevels()
 
-    val Array(zkQuorum, group, topics, numThreads) = args
-    val sparkConf = new SparkConf().setAppName("KafkaWordCount")
+    val Array(zkQuorum, group, topics) = args
+    val sparkConf = new SparkConf().setAppName("Thesis01")
     val ssc = new StreamingContext(sparkConf, Seconds(2))
     ssc.checkpoint("checkpoint")
 
-    val topicMap = topics.split(",").map((_, numThreads.toInt)).toMap
+    val kafkaParams = Map[String, Object](
+      "bootstrap.servers" -> "sky1.it.kth.se:9092",
+      "key.deserializer" -> classOf[StringDeserializer],
+      "value.deserializer" -> classOf[StringDeserializer],
+      "group.id" -> "test",
+      "auto.offset.reset" -> "earliest"
+  //    "enable.auto.commit" -> (false: java.lang.Boolean)
+    )
 
-/* START  */
+    val lines = KafkaUtils.createDirectStream(ssc, PreferConsistent, Subscribe[String, String](Array("test"), kafkaParams))
 
-val kafkaParams = Map[String, Object](
-    "bootstrap.servers" -> "sky1.it.kth.se:9092",
-    "key.deserializer" -> classOf[StringDeserializer],
-    "value.deserializer" -> classOf[StringDeserializer],
-    "group.id" -> "test",
-    "auto.offset.reset" -> "earliest"
-//    "enable.auto.commit" -> (false: java.lang.Boolean)
-  )
+/** WORKING **
+    val words = lines.flatMap(_.value.split(" +"))
+    
 
-  val lines = KafkaUtils.createDirectStream(ssc, PreferConsistent, Subscribe[String, String](Array("test"), kafkaParams))
-
-/* END  */
-
-//    val lines = KafkaUtils.createDirectStream(ssc, zkQuorum, group, topicMap).map(_._2)
-    val words = lines.flatMap(_.value.split(" "))
     val wordCounts = words.map(x => (x, 1L))
       .reduceByKeyAndWindow(_ + _, _ - _, Minutes(10), Seconds(2), 2)
-    wordCounts.print()
+
+    val sortedCounts = wordCounts.map { case(word, count) => (count, word) }
+      .transform(rdd =>  rdd.sortByKey(false) )
+
+    sortedCounts.foreachRDD(rdd =>
+      println("\nTop 10 words:\n" + rdd.take(10).mkString("\n")) )
+***	***/
+
+    val lineWithTemps = lines.map(_.value.split(" +")).map(line => line(9).toLong )
+/*    val sum = lineWithTemps.reduceByWindow( (x: Long, y: Long) => { x+y }, Seconds(30), Seconds(2))
+    val count = lineWithTemps.countByValueAndWindow(Seconds(30), Seconds(2))
+ 
+    val avg: org.apache.spark.streaming.dstream.DStream[Double] = count.transformWith( sum, (rdd1: RDD[Long], rdd2: RDD[Long]) => {
+      rdd2.zip(rdd1).map( tuple => tuple._1/tuple._2 ) 
+    })
+
+    avg.foreachRDD( rdd => println("Average: "+rdd.take(1)(0) ))
+*/
+
+    val windowLength = Minutes(1)
+    val slideInterval = Seconds(2)
+
+    val sum = lineWithTemps.window( windowLength, slideInterval )
+       .foreachRDD( rdd => {
+          val sum = rdd.reduce( _ + _ )
+          val count = rdd.count().toDouble
+	  println("\nSum: "+sum+" | Count: "+count)
+          println("Average: "+ sum/count+"\n" )
+       })
 
     ssc.start()
     ssc.awaitTermination()
@@ -116,11 +143,14 @@ object KafkaWordCountProducer {
       val ts = dfm.parse(date+""+hour+""+min).getTime()
       val timestamp = ts/1000
       //println(timestamp)
-	
-      //val message = new ProducerRecord[String, String]("test", null, timestamp, timestamp.toString, line)
-      val message = new ProducerRecord[String, String]("test", null, null, line)
-
+      
+      val message = new ProducerRecord[String, String]("test", null, timestamp, timestamp.toString, line)
+      //val message = new ProducerRecord[String, String]("test", null, null, line)
+      
+      
+      
       println("real ts: "+timestamp+" | "+message.timestamp())
+      Thread.sleep(2500)
       producer.send(message)
 
 
