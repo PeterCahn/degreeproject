e0aec1bf21a31660a351f9707f86932737474d37
Pietro Cannalire <pietro@sky1.it.kth.se>
2017-06-08 19:22:33 +0200

    Working application: compute average over Kafka topics (centralized, no merge summaries)

--- applications/thesis01/src/main/scala/com/pietro/thesis/KafkaDataExample.scala
+++ applications/thesis01/src/main/scala/com/pietro/thesis/KafkaDataExample.scala
@@ -5,10 +5,11 @@ import java.util.HashMap
 import scala.io.Source
 import scala.collection.immutable.StringOps
 import scala.util.control.Exception
-import java.text.DateFormat;
-import java.text.ParseException;
-import java.text.SimpleDateFormat;
-import java.util.TimeZone;
+import java.text.DateFormat
+import java.text.ParseException
+import java.text.SimpleDateFormat
+import java.util.TimeZone
+import scala.util.Random
 
 import org.apache.kafka.clients.producer.{KafkaProducer, ProducerConfig, ProducerRecord}
 
@@ -25,43 +26,28 @@ import org.apache.spark.rdd.RDD
 import org.apache.log4j._
 import scala.concurrent.duration._
 
-/**
- * Consumes messages from one or more topics in Kafka and does wordcount.
- * Usage: KafkaWordCount <zkQuorum> <group> <topics> <numThreads>
- *   <zkQuorum> is a list of one or more zookeeper servers that make quorum
- *   <group> is the name of kafka consumer group
- *   <topics> is a list of one or more kafka topics to consume from
- *   <numThreads> is the number of threads the kafka consumer should use
- *
- * Example:
- *    `$ bin/run-example \
- *      org.apache.spark.examples.streaming.KafkaWordCount zoo01,zoo02,zoo03 \
- *      my-consumer-group topic1,topic2 1`
- */
 object KafkaWordCount {
   def main(args: Array[String]) {
-    if (args.length < 3) {
-      System.err.println("Usage: KafkaWordCount <zkQuorum> <group> <topics>")
+    if (args.length < 2) {
+      System.err.println("Usage: KafkaWordCount <brokers> <topic>")
       System.exit(1)
     }
 
-//    StreamingExamples.setStreamingLogLevels()
-
-    val Array(zkQuorum, group, topics) = args
+    val Array(brokers, topic) = args
     val sparkConf = new SparkConf().setAppName("Thesis01")
     val ssc = new StreamingContext(sparkConf, Seconds(2))
     ssc.checkpoint("checkpoint")
 
     val kafkaParams = Map[String, Object](
-      "bootstrap.servers" -> "sky1.it.kth.se:9092",
+      "bootstrap.servers" -> brokers,
       "key.deserializer" -> classOf[StringDeserializer],
       "value.deserializer" -> classOf[StringDeserializer],
-      "group.id" -> "test",
-      "auto.offset.reset" -> "earliest"
-  //    "enable.auto.commit" -> (false: java.lang.Boolean)
-    )
+      "group.id" -> "testGroup",
+      "auto.offset.reset" -> "earliest",
+      "enable.auto.commit" -> (false: java.lang.Boolean)
+    )    
 
-    val lines = KafkaUtils.createDirectStream(ssc, PreferConsistent, Subscribe[String, String](Array("test"), kafkaParams))
+    val lines = KafkaUtils.createDirectStream(ssc, PreferConsistent, Subscribe[String, String](topic.split(","), kafkaParams))
 
 /** WORKING **
     val words = lines.flatMap(_.value.split(" +"))
@@ -78,25 +64,17 @@ object KafkaWordCount {
 ***	***/
 
     val lineWithTemps = lines.map(_.value.split(" +")).map(line => line(9).toLong )
-/*    val sum = lineWithTemps.reduceByWindow( (x: Long, y: Long) => { x+y }, Seconds(30), Seconds(2))
-    val count = lineWithTemps.countByValueAndWindow(Seconds(30), Seconds(2))
- 
-    val avg: org.apache.spark.streaming.dstream.DStream[Double] = count.transformWith( sum, (rdd1: RDD[Long], rdd2: RDD[Long]) => {
-      rdd2.zip(rdd1).map( tuple => tuple._1/tuple._2 ) 
-    })
-
-    avg.foreachRDD( rdd => println("Average: "+rdd.take(1)(0) ))
-*/
 
     val windowLength = Minutes(1)
     val slideInterval = Seconds(2)
 
     val sum = lineWithTemps.window( windowLength, slideInterval )
        .foreachRDD( rdd => {
-          val sum = rdd.reduce( _ + _ )
-          val count = rdd.count().toDouble
-	  println("\nSum: "+sum+" | Count: "+count)
-          println("Average: "+ sum/count+"\n" )
+          if(!rdd.isEmpty){
+	    val count = rdd.count().toDouble
+            val sum = rdd.reduce( _ + _ ).toLong
+            println("("+sum+", "+count+", "+sum/count+")")
+         }
        })
 
     ssc.start()
@@ -107,13 +85,12 @@ object KafkaWordCount {
 object KafkaWordCountProducer {
 
   def main(args: Array[String]) {
-    if (args.length < 4) {
-      System.err.println("Usage: KafkaWordCountProducer <metadataBrokerList> <topic> " +
-        "<messagesPerSec> <wordsPerMessage>")
+    if (args.length < 2) {
+      System.err.println("Usage: KafkaWordCountProducer <metadataBrokerList> <topic> ")
       System.exit(1)
     }
 
-    val Array(brokers, topic, messagesPerSec, wordsPerMessage) = args
+    val Array(brokers, topic) = args
     BasicConfigurator.configure()
 
     // Zookeeper connection properties
@@ -128,7 +105,7 @@ object KafkaWordCountProducer {
 
     /* ASOS Surface 1 Minute Data
      * https://www1.ncdc.noaa.gov/pub/data/documentlibrary/tddoc/td3285.pdf */
-    val filename = "../../data/64060K0J4201701.dat"
+    val filename = "../data/64060K0J4201701.dat"
   
     for(line <- Source.fromFile(filename).getLines()) {
       val words = line.split(" +")
@@ -144,19 +121,12 @@ object KafkaWordCountProducer {
       val timestamp = ts/1000
       //println(timestamp)
       
-      val message = new ProducerRecord[String, String]("test", null, timestamp, timestamp.toString, line)
-      //val message = new ProducerRecord[String, String]("test", null, null, line)
+      val message = new ProducerRecord[String, String](topic, null, timestamp, timestamp.toString, line)
       
-      
-      
-      println("real ts: "+timestamp+" | "+message.timestamp())
-      Thread.sleep(2500)
+      println( "ts: "+ts+" | "+ line.replaceAll("\\s+", " ") )
+      Thread.sleep(scala.util.Random.nextInt(10000))
       producer.send(message)
 
-
     }
-
-    
-
   }
 }
