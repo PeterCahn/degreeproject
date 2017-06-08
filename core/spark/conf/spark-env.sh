08f96032480f62c13b4f98d7092f6ef83fa77eeb
Pietro <pietro@sky2.it.kth.se>
2017-06-08 13:37:19 +0200

    Fix format conf/spark-env.sh

--- core/spark/conf/spark-env.sh
+++ core/spark/conf/spark-env.sh
@@ -1,3 +1,24 @@
+export SPARK_LOCAL_IP=sky2.it.kth.se
+export SPARK_MASTER_HOST=sky1.it.kth.se
+export STANDALONE_SPARK_MASTER_HOST=sky1.it.kth.se
+
+export SPARK_WORKER_CORES=16
+export SPARK_WORKER_MEMORY=4g
+export SPARK_WORKER_INSTANCES=2
+
+export SPARK_SSH_FOREGROUND=yes
+
+IGNITE_LIBS="${IGNITE_HOME}/target/release-package/libs/*"
+
+for file in ${IGNITE_HOME}/target/release-package/libs/*
+do
+    if [ -d ${file} ] && [ "${file}" != "${IGNITE_HOME}/target/release-package/libs"/optional ]; then
+        IGNITE_LIBS=${IGNITE_LIBS}:${file}/*
+    fi
+done
+
+export SPARK_CLASSPATH=$IGNITE_LIBS
+
 #!/usr/bin/env bash
 
 #
@@ -25,35 +46,31 @@
 # - HADOOP_CONF_DIR, to point Spark towards Hadoop configuration files
 # - SPARK_LOCAL_IP, to set the IP address Spark binds to on this node
 # - SPARK_PUBLIC_DNS, to set the public dns name of the driver program
-# - SPARK_CLASSPATH, default classpath entries to append
 
 # Options read by executors and drivers running inside the cluster
 # - SPARK_LOCAL_IP, to set the IP address Spark binds to on this node
 # - SPARK_PUBLIC_DNS, to set the public DNS name of the driver program
-# - SPARK_CLASSPATH, default classpath entries to append
 # - SPARK_LOCAL_DIRS, storage directories to use on this node for shuffle and RDD data
 # - MESOS_NATIVE_JAVA_LIBRARY, to point to your libmesos.so if you use Mesos
 
 # Options read in YARN client mode
 # - HADOOP_CONF_DIR, to point Spark towards Hadoop configuration files
-# - SPARK_EXECUTOR_INSTANCES, Number of executors to start (Default: 2)
 # - SPARK_EXECUTOR_CORES, Number of cores for the executors (Default: 1).
 # - SPARK_EXECUTOR_MEMORY, Memory per Executor (e.g. 1000M, 2G) (Default: 1G)
 # - SPARK_DRIVER_MEMORY, Memory for Driver (e.g. 1000M, 2G) (Default: 1G)
 
 # Options for the daemons used in the standalone deploy mode
-export SPARK_MASTER_HOST=sky1.it.kth.se # to bind the master to a different IP address or hostname
+# - SPARK_MASTER_HOST, to bind the master to a different IP address or hostname
 # - SPARK_MASTER_PORT / SPARK_MASTER_WEBUI_PORT, to use non-default ports for the master
 # - SPARK_MASTER_OPTS, to set config properties only for the master (e.g. "-Dx=y")
-export SPARK_WORKER_CORES=16 # to set the number of cores to use on this machine
-export SPARK_WORKER_MEMORY=4g # to set how much total memory workers have to give executors (e.g. 1000m, 2g)
+# - SPARK_WORKER_CORES, to set the number of cores to use on this machine
+# - SPARK_WORKER_MEMORY, to set how much total memory workers have to give executors (e.g. 1000m, 2g)
 # - SPARK_WORKER_PORT / SPARK_WORKER_WEBUI_PORT, to use non-default ports for the worker
-export SPARK_WORKER_INSTANCES=4 # to set the number of worker processes per node
-#export SPARK_WORKER_DIR=/home/sparkdata # to set the working directory of worker processes
+# - SPARK_WORKER_DIR, to set the working directory of worker processes
 # - SPARK_WORKER_OPTS, to set config properties only for the worker (e.g. "-Dx=y")
 # - SPARK_DAEMON_MEMORY, to allocate to the master, worker and history server themselves (default: 1g).
 # - SPARK_HISTORY_OPTS, to set config properties only for the history server (e.g. "-Dx=y")
-#export SPARK_SHUFFLE_OPTS="-Dspark.deploy.defaultCores 16" # to set config properties only for the external shuffle service (e.g. "-Dx=y")
+# - SPARK_SHUFFLE_OPTS, to set config properties only for the external shuffle service (e.g. "-Dx=y")
 # - SPARK_DAEMON_JAVA_OPTS, to set config properties for all daemons (e.g. "-Dx=y")
 # - SPARK_PUBLIC_DNS, to set the public dns name of the master or workers
 
@@ -64,21 +81,3 @@ export SPARK_WORKER_INSTANCES=4 # to set the number of worker processes per node
 # - SPARK_IDENT_STRING  A string representing this instance of spark. (Default: $USER)
 # - SPARK_NICENESS      The scheduling priority for daemons. (Default: 0)
 # - SPARK_NO_DAEMONIZE  Run the proposed command in the foreground. It will not output a PID file.
-export SPARK_SSH_FOREGROUND=yes
-export STANDALONE_SPARK_MASTER_HOST=sky1.it.kth.se
-
-IGNITE_LIBS="${IGNITE_HOME}/target/release-package/libs/*"
-
-for file in ${IGNITE_HOME}/target/release-package/libs/*
-do
-    if [ -d ${file} ] && [ "${file}" != "${IGNITE_HOME}/target/release-package/libs"/optional ]; then
-        IGNITE_LIBS=${IGNITE_LIBS}:${file}/*
-    fi
-done
-
-
-#IGNITE_LIBS="${IGNITE_LIBS}:home/pietro/spark/kafka-0-8-assembly/target/spark-streaming-kafka-0-8-assembly_2.11-2.3.0-SNAPSHOT.jar"
-
-
-export SPARK_CLASSPATH=$IGNITE_LIBS
-
