0b14d070c77be7dee785f6cc83a7c93f19c12d00
Pietro Cannalire <pietro@sky1.it.kth.se>
2017-06-17 20:48:59 +0200

    Add publication of messages to Kafka topic loading previous ones to fit a windowLength time. Then publication every minute.

--- applications/thesis01/src/main/scala/com/pietro/thesis/KafkaDataExample.scala
+++ applications/thesis01/src/main/scala/com/pietro/thesis/KafkaDataExample.scala
@@ -30,21 +30,26 @@ import org.apache.spark.rdd.RDD
 import org.apache.log4j._
 import scala.concurrent.duration._
 
-object KafkaWordCount {
-  def main(args: Array[String]) {
-    if (args.length < 2) {
-      System.err.println("Usage: KafkaWordCount <brokers> <topic>")
+object ComputeClusterLocal {
+  def main(args: Array[String]) {    
+
+    if (args.length < 3) {
+      System.err.println("Usage: KafkaWordCount <brokers> <topic> <windowLength (in minutes)>")
       System.exit(1)
     }
-
+    
+    /* TO DO:
+     * ParameteTopics and brokers parameter can be also taken from configuration files */
+//    val inputKafkaTopic = "dataSky1"
     val outputKafkaTopic = "windowedSummaries"
-
-    val Array(brokers, topic) = args
+//    val brokers = "sky1.it.kth.se:9092,sky2.it.kth.se:9092"
+   
+    val Array(brokers, inputKafkaTopic, winLength) = args
     val sparkConf = new SparkConf().setAppName("Thesis01")
-    val ssc = new StreamingContext(sparkConf, Seconds(2))
+    val ssc = new StreamingContext(sparkConf, Seconds(5))
     ssc.checkpoint("dataAggregation")
 
-    val topics = topic.split(",").toList
+    val topics = inputKafkaTopic.split(",").toList
     val kafkaParams = Map[String, Object](
       "bootstrap.servers" -> brokers,
       "key.deserializer" -> classOf[StringDeserializer],
@@ -53,42 +58,43 @@ object KafkaWordCount {
       "auto.offset.reset" -> "earliest",
       "enable.auto.commit" -> (true: java.lang.Boolean)
     )    
-    //val offsets = Map(new TopicPartition(topic, 0) -> 2L)
-
+    
+    /* Create stream from Kafka topics */
     val lines = KafkaUtils.createDirectStream(ssc, PreferConsistent, Subscribe[String, String](topics, kafkaParams))
 
+    /* Extract only useful information from known line schema */
     val lineWithTemps = lines.map(_.value.split(" +"))
-	.filter(line => line.length > 8)
-	.map(line => line(9).toLong )
+	.filter(line => line.length > 8) // prevent lines with less than 8 word: wrong or not supported format (in this version)
+	.map(line => line(9).toLong )    // map each line to a temperature
 
-    val windowLength = Minutes(1)
-    val slideInterval = Seconds(2)
+    val windowLength = Minutes(winLength.toInt)
+    val slideInterval = Seconds(5)
 
     lineWithTemps.window( windowLength, slideInterval )
        .foreachRDD( rdd => {
-          if( !rdd.isEmpty){
+          if( !rdd.isEmpty ){
 
-	    /* Compute sum and count */
+	    /* Compute sum and count on each RDD */
 	    val count: Double = rdd.count().toDouble
             val sum: Long = rdd.reduce( _ + _ ).toLong
 		
             /* Prepare to send message to Kafka topic  */
  	    val props = new HashMap[String, Object]()
-	    props.put(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, brokers)
+	    props.put(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, "sky1.it.kth.se:9092,sky2.it.kth.se:9092")
 	    props.put(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, "org.apache.kafka.common.serialization.StringSerializer")
 	    props.put(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, "org.apache.kafka.common.serialization.StringSerializer")
 	    val producer = new KafkaProducer[String, String](props)
 	    
    	    /* Prepare data format to put into the message */
-	    val format = new SimpleDateFormat("yyyyMMddkkmm")
+	    val format = new SimpleDateFormat("yyyyMMddkkmmss")
 	    val date = format.format(Calendar.getInstance().getTime())                
 		  
- 	    val data = date+", "+sum+", "+count+", "+sum/count+", "+windowLength+", 1"
-	    val message = new ProducerRecord[String, String](outputKafkaTopic, null, data)	  
+ 	    val data = date+", "+sum+", "+count+", "+sum/count+", "+windowLength+", 1" // record to publish
+	    val message = new ProducerRecord[String, String](outputKafkaTopic, null, data)
 	    
-	    println(data)   	    
-	
-	    producer.send(message)
+	    println(data) // debug purposes only
+
+	    producer.send(message) // publish message into outputKafkaTopic
 	    producer.close()
           }
        })
@@ -103,7 +109,7 @@ object Reconcile {
     if (args.length < 1) {
       System.err.println("Usage: KafkaWordCount <brokers>")
       System.exit(1)
-    }
+    }    
     
     val inputKafkaTopic = "windowedSummaries"
 
@@ -125,23 +131,28 @@ object Reconcile {
 
     val aggregationByTime = records.map( record => {
       val recordArr = record.value.trim.split(",")
-      ( recordArr(0).toLong, recordArr.drop(1).mkString(", ") )
+      val key: Long = Try( recordArr(0).toLong ).toOption.getOrElse(-1)
+      ( key, recordArr.drop(1).mkString(", ") )
     })
     .reduceByKey( (v1, v2) => {
 	val vArr1 = v1.trim.split(", +")
 	val vArr2 = v2.trim.split(", +")
 	val sum = vArr1(0).toDouble + vArr2(0).toDouble
 	val count = vArr1(1).toDouble + vArr2(1).toDouble
-	println(sum+", "+count+", "+sum/count)
-        sum+", "+count+", "+sum/count+", "+vArr1(3)
+        " "+sum+", "+count+", "+sum/count+", "+vArr1(3)+", "+vArr1(4)+"|"+vArr2(4)
     })
+    .transform( rdd => rdd.sortByKey(false) )
+    //.take(10)
 
+/*
     val onlyLastTimeAggregated = aggregationByTime.reduce( (x, y) => { 
         val maximumTs = max(x._1, y._1)
 	if( maximumTs == x._1) x else y
     })
+*/
 
-    onlyLastTimeAggregated.print()    
+    //onlyLastTimeAggregated.print()    
+    aggregationByTime.print()
 
     ssc.start()
     ssc.awaitTermination()
@@ -149,7 +160,7 @@ object Reconcile {
 }
 
 
-object KafkaWordCountProducer {
+object DataProducer {
 
   def main(args: Array[String]) {
     if (args.length < 2) {
@@ -171,35 +182,27 @@ object KafkaWordCountProducer {
     /* ASOS Surface 1 Minute Data
      * https://www1.ncdc.noaa.gov/pub/data/documentlibrary/tddoc/td3285.pdf */
     val filename = "../data/64060K0J4201701.dat"
-  
-//    for(line <- Source.fromFile(filename).getLines().filter( line => !safeStringToLong(line.split(" +")(9)).isEmpty  )){
-    for(line <- Source.fromFile(filename).getLines().filter( line => !Try( line.split(" +")(9).toLong ).toOption.isEmpty  )){
-      val words = line.split(" +")
-      //if( Some(words(9).toLong).getOrElse("NO") == "NO" ) break
-      val date = words(1).slice(3,11)
-      val hour = words(1).slice(11,13)
-      val min  = words(1).slice(13,15)
-      //println(date+","+hour+":"+min)
+    
+//    val format = new SimpleDateFormat("yyyyMMddkkmmss")
+//    val dateTime = Calendar.getInstance().getTime()
+    val swTs = (System.currentTimeMillis / 1000) + 7200 // adding 2 hours (3600 s * 2)
+    var ts = swTs - 1200 // previous 20 minutes (5 min = 300 s => 20 min = 1200 s)
 
-      /* build timestamp from date */
-      val dfm = new SimpleDateFormat("yyyyMMddHHmm")
-      dfm.setTimeZone(TimeZone.getTimeZone("GMT+1:00"))
-      val ts = dfm.parse(date+""+hour+""+min).getTime()
-      val timestamp = ts/1000
-      //println(timestamp)
+    for(line <- Source.fromFile(filename).getLines().filter( line => !Try( line.split(" +")(9).toLong ).toOption.isEmpty  )){
+      val now = ((System.currentTimeMillis /1000) + 7200)
+      if ( ts >= now ) Thread.sleep(60000)
+
+      val words = line.split(" +")
+      val eventTime = words(1).slice(3,15)      
+
+      val message = new ProducerRecord[String, String](topic, null, ts, ts.toString, line)
       
-      val message = new ProducerRecord[String, String](topic, null, timestamp, timestamp.toString, line)
-      
-      println( "ts: "+ts+" | "+ line.replaceAll("\\s+", " ") )
-      Thread.sleep(scala.util.Random.nextInt(2000))
+      println( line.replaceAll("\\s+", " ") )
       producer.send(message)
+      producer.close()
+      ts = ts + 60
 
     }
   }
-
-      def safeStringToLong(str: String): Option[Long] = {
-        catching(classOf[NumberFormatException]) opt str.toLong
-}
-
 
 }
