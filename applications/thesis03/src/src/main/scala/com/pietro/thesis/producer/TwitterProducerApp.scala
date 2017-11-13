09e1c107a75dfd7d677f9e4a9456fa085fd8aa27
Pietro Cannalire <pietro@sky1.it.kth.se>
2017-11-13 18:06:45 +0100

    Cluster local MG computation done. Added test setup with dynamic schema for testing info.

---
+++ applications/thesis03/src/src/main/scala/com/pietro/thesis/producer/TwitterProducerApp.scala
@@ -0,0 +1,10 @@
+package com.htvu.producer
+
+object TwitterProducerApp {
+
+  def main (args: Array[String]) {
+    val twitterProducer = new TwitterProducer with StringKafkaProducer
+
+    twitterProducer.start(args)
+  }
+}
